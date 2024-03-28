9013b5a554cc3d6972b09a21905c5d908b4513f2
Connor Heard <44000070+Conman2288@users.noreply.github.com>
2024-03-27 22:51:46 -0500

    Added Cyclic_key function
    Added a function that uses modulo math to repeat the chars in the key up to the length of the user provided text.

--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -3,7 +3,16 @@
 
 import sys
 
-# Function to encrypt or decrypt the user text with the provided key
+# This function output a cyclic key used in Vigenere cipher
+def cyclic_key_generation(string, key):
+    key = list(key)
+    if len(string) == len(key):
+        return key
+    else:
+        for i in range (len(string) - len(key)):
+            key.append(key[i % len(key)])
+            return("", join(key))
+
 
 # Checking if the user gives correct number and types of arguments
 if not((len(sys.argv) >= 2 and (sys.argv[1] == "-d" or sys.argv[1] == "-e"))):
@@ -11,4 +20,9 @@ if not((len(sys.argv) >= 2 and (sys.argv[1] == "-d" or sys.argv[1] == "-e"))):
     sys.exit()
 
 else:
+    print("Here is the given key " + sys.argv[2])
+
     user_text = input("\n")
+
+    # Create a repeatable key with equal length to user provided string
+    full_key = cyclic_key_generation(user_text, sys.argv[2])
